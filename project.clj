d29e4b6fd75054c6a69da7966a97954829f6f6a5
rod <rod@pu-gh.com>
2013-04-24 18:32:33 +0100

    version 0.3.0

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 
-(defproject confo "0.2.0"
+(defproject confo "0.3.0"
   :description "Easy Ambient Config"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
