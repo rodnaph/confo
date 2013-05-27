5bdcf4c337e37e8bdc3e269b16d81e333a7acd75
rod <rod@pu-gh.com>
2013-05-27 17:13:37 +0100

    bump clojure to 1.5.1

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 
 (defproject confo "0.5.0"
   :description "Easy Ambient Config"
-  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
   :plugins [[lein-midje "3.0.1"]])
 
