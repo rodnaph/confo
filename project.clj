003c70527e5fa44bab3af9ecbf041712953c5bd1
rod <rod@pu-gh.com>
2013-08-04 10:19:46 +0100

    updated midje

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :description "Easy Ambient Config"
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]}}
-  :plugins [[lein-midje "3.0.1"]])
+  :plugins [[lein-midje "3.1.1"]])
 
